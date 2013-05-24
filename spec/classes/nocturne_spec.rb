b9921fb616a770f3915e6246bb386f20e7d2d674
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-05-24 17:38:04 +0200

    update

---
+++ spec/classes/nocturne_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+# Rename this file to classname_spec.rb
+# Check other boxen modules for examples
+# or read http://rspec-puppet.com/tutorial/
+describe 'nocturne' do
+	should contain_package('Nocturne').with({
+	   :provider => 'compressed_app',
+	   :source   => 'https://github.com/downloads/InBeta/nocturne/Nocturne.zip'
+	})
+end
